9836df0aea4f0f39c65feb4ffbd6a7f0b4cef39e
Parth <work.parthm257@gmail.com>
2023-11-01 20:00:20 +0530

    init

---
+++ src/app.module.ts
@@ -0,0 +1,46 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ChatModule } from './chat/chat.module';
+import { AuthMiddleware } from './middleware/auth.middleware';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://Aegon:LZhaFxpexFJOgJ8J@cluster0.opxlunk.mongodb.net/demoDB',
+      autoLoadEntities: true,
+      connectTimeoutMS: 40000,
+      logger: 'simple-console',
+      synchronize: true,
+      retryAttempts: 5,
+      retryDelay: 5000,
+    }),
+    UserModule,
+    ChatModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude(
+        { path: '/users', method: RequestMethod.POST },
+        { path: '/users/login', method: RequestMethod.POST },
+      )
+      .forRoutes('');
+  }
+}
