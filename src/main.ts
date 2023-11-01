9c68acd442b089ffcd62e061b67237a35c3ea09c
Parth <work.parthm257@gmail.com>
2023-11-01 22:31:07 +0530

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder().setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
